b2d6c66ac2c5643e262acc0a17e4e8eccfc570fd
ash-valley <42672817+ash-valley@users.noreply.github.com>
2022-11-18 10:43:57 +0900

    feat(youtube再生できるところまで):

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,24 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import YouTube, { YouTubeProps } from "react-youtube";
 
 export default function Home() {
+  const _onReady: YouTubeProps["onReady"] = (event) => {
+    event.target.loadVideoById({
+      videoId: "HxJoNjbJHjY", //YouTubeのvideo ID
+      startSeconds: 10, //開始時間。秒で記述
+      endSeconds: 20, //終了時間。秒で記述
+    });
+  };
+  const _onPlay: YouTubeProps["onPlay"] = (event) => {
+    // while (true) {
+    //   if (event.target.getPlayerState() !== 1) {
+    //     break;
+    //   }
+    //   console.log(event.target.getCurrentTime());
+    // }
+  };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,60 +28,10 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <YouTube videoId={"HxJoNjbJHjY"} onReady={_onReady} onPlay={_onPlay} />
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <footer className={styles.footer}></footer>
     </div>
-  )
+  );
 }
