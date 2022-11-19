a255b2cb24295277b523c3fc2c61db3528ab7f0d
ash-valley <42672817+ash-valley@users.noreply.github.com>
2022-11-19 17:55:06 +0900

    feat: chakra ui追加

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,16 @@
 import "../styles/globals.css";
 import { QueryClient, QueryClientProvider, useQuery } from "react-query";
 import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
 
 const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
-    </QueryClientProvider>
+    <ChakraProvider>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </ChakraProvider>
   );
 }
